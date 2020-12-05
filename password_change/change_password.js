ddbb5d06c88ca7b9f060498aec6a0a03c7b2b612
rakki-18 <rakki22k@gmail.com>
2020-12-06 01:46:25 +0530

    fixed display in password change

--- password_change/change_password.js
+++ password_change/change_password.js
@@ -1,21 +1,25 @@
-function api_get_call()
-{
-    $( document ).ready(function(){
-    
-    var details = document.getElementById("frm");
-    alert("fucker");
-    to_url = 'https://em7ialrwbi.execute-api.ap-south-1.amazonaws.com/demo' + '?old=' + details.old.value + '&new=' + details.new.value + '&con=' + details.confirm.value;
+
+function myfunc(){
+    var to_url = "https://35i970cxs5.execute-api.ap-south-1.amazonaws.com/demo/password" + "?old=" + frm.old.value + "&new=" + frm.new.value + "&con=" + frm.confirm.value;
+        
     $.ajax({
-        url: to_url;
+        url: to_url,
         type: 'get',
         success: function(data){
-            $('#response').html(data);
+            
+            if(data == 1)
+             response = "wrong password";
+            else if(data == 2)
+             response = "passwords don't match";
+            else
+             response = "updated successfully";
+            $('#response').html(response);
+            
         },
         error: function (xhr, ajaxOptions, thrownError) {
             var errorMsg = 'Ajax request failed: ' + xhr;
             console.log(thrownError);
             $('#response').html(errorMsg);
-          }
+        }
     });
-});
 }
