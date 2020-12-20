75ab9ea9bcd80b3fd7dfef57c59fb6672f50fc23
rakki-18 <rakki22k@gmail.com>
2020-12-20 17:21:06 +0530

    modify directory structure

---
+++ signup.js
@@ -0,0 +1,37 @@
+function encrypt(test_string){
+    var encrypted_string = "";
+    for(let i= 0; i < test_string.length;i++)
+    {
+        encrypted_string+=(test_string.charCodeAt(i) + 100).toString();
+    }
+    return encrypted_string;
+}
+
+function signup(){
+    var  encrypted_username = encrypt(frm.username.value);
+    var encrypted_passwd = encrypt(frm.passwd.value);
+    var confirm_passwd = encrypt(frm.confirm.value);
+
+    var to_url = "https://fldz51d8j6.execute-api.ap-south-1.amazonaws.com/demo/signup" + "?username=" + encrypted_username + "&passwd=" + encrypted_passwd
+    + "&otp=" + frm.otp.value+ "&branch=" + frm.branch.value+ "&Phone=" + frm.phone.value
+    + "&email=" + frm.email.value+ "&confirm=" + confirm_passwd;
+    
+    $.ajax({
+        url: to_url,
+        type: 'get',
+        success: function(data){
+            
+
+            
+            var response_htm = '<div id = "response_css">' + data + '</div>';
+            $('#response').html(response_htm);
+            if(data == "data updated")
+            location.href = "/profile_page/profile.htm"; 
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            var errorMsg = 'Ajax request failed: ' + xhr;
+            console.log(thrownError);
+            $('#response').html(errorMsg);
+        }
+    });
+}
